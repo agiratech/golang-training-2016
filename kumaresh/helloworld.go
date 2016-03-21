b5578a963dd4b715df1c26b76f7eda7f8082dc24
agiratech-kumaresanj <kumaresan@agiratech.com>
2016-03-21 14:10:12 +0530

    Sample Helloworld

---
+++ kumaresh/helloworld.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+
+func main() {
+
+  var sample string 
+  var samplesecond string
+  sample = "Hello world"
+  fmt.Println(sample)
+  samplesecond = "Me too Hello world "
+  fmt.Println(samplesecond)
+
+ }
