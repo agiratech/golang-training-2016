4b6e36c50d27fa6c85272892896bd0208cdf44c0
Arun Kumar G <arunkumar@agiratech.com>
2016-03-22 18:15:32 +0530

    Modified Persondetails

--- arunkumar/persondetails.go
+++ arunkumar/persondetails.go
@@ -4,13 +4,8 @@ import "fmt"
 
 var n int
 var i int
-<<<<<<< HEAD
 var average int
 type Student struct{
-=======
-
-type Student struct {
->>>>>>> 73576cc831ecb76bc573ecc24d32ec359b155122
   
   Name string
   RollNo int
@@ -22,7 +17,6 @@ func studentdetails() Student {
 var stu Student
 fmt.Println("Enter Number of Students")
 fmt.Scanf("%d", &n)
-for i = 0; i < n ; i++{
 fmt.Println("Enter Student Name : ")
 fmt.Scanf("%s", &stu.Name)
 fmt.Println("Enter Student Roll-No : ")
@@ -31,16 +25,14 @@ fmt.Println("Enter Student Subjects : ")
 fmt.Scanf("%d", &stu.Subjects)
 fmt.Println("Enter Student Over-all_Marks : ")
 fmt.Scanf("%d", &stu.OverallMarks)
-
-fmt.Println("Your Total Subjects average Marks is : ",average)
 average = stu.OverallMarks/stu.Subjects
-}
+fmt.Println("Your Total Subjects average Marks is : ",average)
 return stu  
 }
  
 func main() {
  var a Student 
   a = studentdetails()
-	fmt.Println("Hi ",a.Name,"(",a.RollNo,") You got ",a.OverallMarks,"in ",a.Subjects)
+	fmt.Println("Hi ",a.Name,"(",a.RollNo,") Your average is",average,"in ",a.Subjects,"Subjects")
   
 }
