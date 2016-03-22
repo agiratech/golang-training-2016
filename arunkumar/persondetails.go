c652db87ca56e45b68b9b93e6e8ec7ad203d1e09
Arun Kumar G <arunkumar@agiratech.com>
2016-03-22 17:56:52 +0530

    changes for merge

--- arunkumar/persondetails.go
+++ arunkumar/persondetails.go
@@ -4,38 +4,38 @@ import "fmt"
 
 var n int
 var i int
-
+var average int
 type Student struct{
   
   Name string
   RollNo int
-  Subjects string 
+  Subjects int 
   OverallMarks int
 }
 
-func studentdetails() Student{
+func studentdetails() Student {
 var stu Student
 fmt.Println("Enter Number of Students")
 fmt.Scanf("%d", &n)
-for i = 0; i > n ; i++{
+for i = 0; i < n ; i++{
 fmt.Println("Enter Student Name : ")
 fmt.Scanf("%s", &stu.Name)
 fmt.Println("Enter Student Roll-No : ")
 fmt.Scanf("%d", &stu.RollNo)
 fmt.Println("Enter Student Subjects : ")
-fmt.Scanf("%s", &stu.Subjects)
+fmt.Scanf("%d", &stu.Subjects)
 fmt.Println("Enter Student Over-all_Marks : ")
 fmt.Scanf("%s", &stu.OverallMarks)
 
-//fmt.Println("Your Total Subjects average Marks is : ",average)
+fmt.Println("Your Total Subjects average Marks is : ",average)
+average = stu.OverallMarks/stu.Subjects
 }
-return stu 
+return stu  
 }
  
 func main() {
  
- var a Student 
-
-  a = studentdetails()
+ var b Student 
+ b = studentdetails()
   
 }
