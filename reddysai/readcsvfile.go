f7de75df0af3c875a7721f6a30c3e9ea6b6715e0
reddysai <reddysai@agiratech.com>
2016-04-05 14:45:58 +0530

    modified file for reading csv file records

--- reddysai/readcsvfile.go
+++ reddysai/readcsvfile.go
@@ -39,9 +39,9 @@ func main() {
 		os.Exit(1)
 	}
 	//display the standard output
-	for _, each := range rawCSVdata {
+	/*for _, each := range rawCSVdata {
 			fmt.Printf("%s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\n",each[0],each[1],each[2],each[3],each[4],each[5],each[6],each[7],each[8],each[9],each[10],each[11],each[12],each[13],each[14],each[15],each[16],each[17])
-	}
+	}*/
 	//now store the date into the struct
 	var onerecord TestRecord
 	var allrecords []TestRecord
@@ -70,4 +70,35 @@ func main() {
 
 	}
 	fmt.Println(allrecords)
+	insertion(allrecords)
+}
+
+func insertion(allrecords []TestRecord) {
+
+	var squery string = "insert into myfile_sample(policyid,statecode,country,eqsitelimit,husitelimit,flsitelimit,frsitelimit,tiv_2011,tiv_2012,eqsitedeductible,husitedeductible,flsitedeductible,frsitedeductible,point_latitude,point_langitude,line,construction,point_granularity) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$1,$11,$12,$13,$14,$15,$16,$17,$18)"
+
+	db, err := sql.Open("postgres","user=postgres password='root' dbname=sai sslmode=disable")
+
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("StartTime :",time.Now())
+
+	for i:= range allrecords{
+		query, err := db.Prepare(squery)
+
+		if err != nil{
+			log.Fatal(err)
+		}
+		
+		res, err := query.Exec(allrecords[i].policyID,allrecords[i].statecode,allrecords[i].country,allrecords[i].eqsiteLimit,allrecords[i].husiteLimit,allrecords[i].flsiteLimit,allrecords[i].frsiteLimit,allrecords[i].tiv_2011,allrecords[i].tiv_2012,allrecords[i].eqsitedeductible,allrecords[i].husitedeductible,allrecords[i].flsitedeductible,allrecords[i].frsitedeductible,allrecords[i].point_latitude,allrecords[i].point_langitude,allrecords[i].line,allrecords[i].construction,allrecords[i].point_granularity)
+
+		if err!=nil || res == nil{
+			log.Fatal()
+		}
+		query.Close()
+	}
+	db.Close()
+
+	fmt.Println("StopTime :",time.Now())
 }
