0b5e093cd776a34d073a564f60d92514cfb4e2c4
reddysai <reddysai@agiratech.com>
2016-04-20 20:58:18 +0530

    modified insertion test file

--- csv/insertdynamic/Insertion_test.go
+++ csv/insertdynamic/Insertion_test.go
@@ -14,15 +14,16 @@ type details struct{
 func Test_Insert(t *testing.T){
 	//k:="{Empid:301,Name:sai,Age:22,Address:tirupati,Salary:₹ 10,000.00}"
 	var d details
-	d.Empid=303
+	d.Empid=305
 	d.Name="sai"
 	d.Age=22
 	d.Address="tpt"
 	d.Salary=10000
 	js,_:= json.Marshal(d)
 	js1:=string(js)
-	s1,_:= Insert(js1)
-if s1=="Inserted Successfully"{
+	s1,val:= Insert(js1)
+	t.Log(val)
+if s1=="Inserted Successfully" && val==`{"Empid":"305","Name":"sai","Age":"22","Address":"tpt","Salary":"10000"}`{
 	t.Log("Its working")
 } else{
 	t.Error("Its not working")
